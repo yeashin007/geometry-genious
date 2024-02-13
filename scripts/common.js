39e0b4f3bdc1d6c8a49aa6eb072f39e9dfe6b5bd
yeashin007 <yeashin007@gmail.com>
2024-02-13 22:22:19 +0600

    Validation done

--- scripts/common.js
+++ scripts/common.js
@@ -9,20 +9,28 @@ const p1 = document.createElement("p");
 const p2 = document.createElement("p");
 const p3 = document.createElement("p");
 
-function showCalculation(para1, valu1, para2, value2, area, areaValue) {
-  const select = document.querySelector("#showCalc");
+function showCalculation(para1, value1, para2, value2, area, areaValue) {
+  p1.textContent = "";
+  p2.textContent = "";
+  p3.textContent = "";
 
-  p1.textContent = `${para1}: ${valu1}`;
-  p1.style.fontSize = "25px";
+  if (isNaN(value1) || isNaN(value2) || value1 < 0 || value2 < 0) {
+    alert("Enter valid number 🚫");
+  } else {
+    const select = document.querySelector("#showCalc");
 
-  p2.textContent = `${para2}: ${value2}`;
-  p2.style.fontSize = "25px";
+    p1.textContent = `${para1}: ${value1}`;
+    p1.style.fontSize = "25px";
 
-  p3.textContent = `${area}: ${areaValue}`;
-  p3.style.fontSize = "25px";
-  p3.style.fontWeight = "700";
+    p2.textContent = `${para2}: ${value2}`;
+    p2.style.fontSize = "25px";
 
-  select.appendChild(p1);
-  select.appendChild(p2);
-  select.appendChild(p3);
+    p3.textContent = `${area}: ${areaValue}`;
+    p3.style.fontSize = "25px";
+    p3.style.fontWeight = "700";
+
+    select.appendChild(p1);
+    select.appendChild(p2);
+    select.appendChild(p3);
+  }
 }
